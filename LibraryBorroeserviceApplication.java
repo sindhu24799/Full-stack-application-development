b4c3e86a8e6f86e795d3ca28daa5ca4c3d17a29b
chevella sindhu <sinduchevella@gmail.com>
2026-04-21 09:46:58 +0530

    added week7-microservices

---
+++ LibraryBorroeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.borrowservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBorroeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBorroeserviceApplication.class, args);
+	}
+
+}
---
+++ LibraryRegistryApplication.java
@@ -0,0 +1,22 @@
+package com.example.springbot_demo1;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class LibraryRegistryApplication {
+
+public static void main(String[] args){
+
+SpringApplication.run(
+LibraryRegistryApplication.class,
+args);
+
+}
+
+}
