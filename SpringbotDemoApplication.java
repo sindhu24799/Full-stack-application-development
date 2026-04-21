14d0d5be68894955537061ccc163628d5d6c1965
chevella sindhu <sinduchevella@gmail.com>
2026-04-21 09:48:21 +0530

    added week11-using jenkins

---
+++ SpringbotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotDemoApplication.class, args);
+	}
+
+}
